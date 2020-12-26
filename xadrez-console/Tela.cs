46ee97ed3487dbdb03d82add6f24ca2e73521e15
Rafael Silva <rafaeldeoliveira88@bol.com.br>
2020-12-26 12:51:32 -0300

    Fazendo os primeiros testes de movimentacao

--- xadrez-console/Tela.cs
+++ xadrez-console/Tela.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using tabuleiro;
+using xadrez;
 
 namespace xadrez_console
 {
@@ -9,7 +10,7 @@ namespace xadrez_console
 
             for (int i=0; i < tab.linhas; i++) {
                 Console.Write(8 - i + " ");
-                for (int j = 0; j < tab.colunas; j++) {
+                for (int j=0; j < tab.colunas; j++) {
                     if (tab.peca(i, j) == null)
                     {
                         Console.Write("- ");
@@ -21,7 +22,7 @@ namespace xadrez_console
                 }
                 Console.WriteLine();
             }
-            Console.WriteLine(" a b c d e f g h");
+            Console.WriteLine("  a b c d e f g h");
         }
 
         public static void imprimirPeca(Peca peca) {
@@ -37,6 +38,12 @@ namespace xadrez_console
             }
         }
 
+        public static PosicaoXadrez lerPosicaoXadrez() {
+            string s = Console.ReadLine();
+            char coluna = s[0];
+            int linha = int.Parse(s[1] + "");
+            return new PosicaoXadrez(coluna, linha);
+        }
 
     }
 }
--- xadrez-console/tabuleiro/Peca.cs
+++ xadrez-console/tabuleiro/Peca.cs
@@ -18,6 +18,9 @@ namespace tabuleiro
             this.qtdMovimentos = 0;
         }
 
+        public void incrementarQtdMovimentos() {
+            qtdMovimentos++;
+        }
 
     }
 }
