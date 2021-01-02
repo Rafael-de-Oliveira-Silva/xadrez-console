b9166bb61fdbd3237023b7a0e2ab3a4feadb762d
Rafael Silva <rafaeldeoliveira88@bol.com.br>
2021-01-02 16:55:47 -0300

    Implementando a verificacao de xeque

--- xadrez-console/xadrez/PartidaDeXadrez.cs
+++ xadrez-console/xadrez/PartidaDeXadrez.cs
@@ -9,19 +9,23 @@ namespace xadrez
         public int turno { get; private set; }
         public Cor jogadorAtual { get; private set; }        
         public bool terminada { get; private set; }
+        public bool xeque { get; private set; }
         public HashSet<Peca> pecas;
         public HashSet<Peca> capturadas;
+        
 
         public PartidaDeXadrez() {
             tab = new Tabuleiro(8,8);
             turno = 1;
+            terminada = false;
+            xeque = false;
             jogadorAtual = Cor.Branca;
             pecas = new HashSet<Peca>();
             capturadas = new HashSet<Peca>();
             colocarPecas();
         }
 
-        public void executaMovimento(Posicao origem, Posicao destino) {
+        public Peca executaMovimento(Posicao origem, Posicao destino) {
             Peca p = tab.retirarPeca(origem);
             p.incrementarQtdMovimentos();
             Peca pecaCapturada = tab.retirarPeca(destino);
@@ -29,10 +33,35 @@ namespace xadrez
             if (pecaCapturada != null) {
                 capturadas.Add(pecaCapturada);
             }
+            return pecaCapturada;
+        }
+
+        public void desfazMovimento(Posicao origem, Posicao destino, Peca pecaCapturada) {
+            Peca p = tab.retirarPeca(destino);
+            p.decrementarQtdMovimentos();
+            if (pecaCapturada != null) {
+                tab.colocarPeca(pecaCapturada, destino);
+                capturadas.Remove(pecaCapturada);
+            }
+            tab.colocarPeca(p, origem);
         }
 
         public void realizaJogada(Posicao origem, Posicao destino) {
-            executaMovimento(origem, destino);
+            Peca pecaCapturada = executaMovimento(origem, destino);
+
+            if (estahEmXeque(jogadorAtual)) {
+                desfazMovimento(origem, destino, pecaCapturada);
+                throw new TabuleiroException("Você não pode se colocar em xeque!");
+            }
+
+            if (estahEmXeque(adversaria(jogadorAtual)))
+            {
+                xeque = true;
+            }
+            else {
+                xeque = false;
+            }
+
             turno++;
             mudaJogador();
         }
@@ -111,6 +140,41 @@ namespace xadrez
             colocarNovaPeca('e', 8, new Torre(tab, Cor.Preta));
             colocarNovaPeca('d', 8, new Rei(tab, Cor.Preta));
 
+        }
+
+        private Cor adversaria(Cor cor) {
+            if (cor == Cor.Branca)
+            {
+                return Cor.Preta;
+            }
+            else {
+                return Cor.Branca;
+            }
+        }
+
+        private Peca rei(Cor cor) {
+            foreach (Peca x in pecasEmJogo(cor)) {
+                if (x is Rei) {
+                    return x;
+                }
+            }
+            return null;
+        }
+
+        public bool estahEmXeque(Cor cor) {
+            Peca pecaRei = rei(cor);
+            if (pecaRei == null) {
+                throw new TabuleiroException("Não tem rei da cor " + cor + " no tabuleiro!");
+            }
+
+            foreach (Peca x in pecasEmJogo(adversaria(cor))) {
+                bool[,] mat = x.movimentosPossiveis();
+                if (mat[pecaRei.posicao.linha, pecaRei.posicao.coluna]) {
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
 }
