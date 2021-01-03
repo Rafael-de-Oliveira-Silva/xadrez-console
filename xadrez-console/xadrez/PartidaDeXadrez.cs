952d2caf88a848bff61ddfe9218545956fce9674
Rafael Silva <rafaeldeoliveira88@bol.com.br>
2021-01-03 14:35:42 -0300

    Jogada Especial - Promoção

--- xadrez-console/xadrez/PartidaDeXadrez.cs
+++ xadrez-console/xadrez/PartidaDeXadrez.cs
@@ -54,6 +54,20 @@ namespace xadrez
                 throw new TabuleiroException("Você não pode se colocar em xeque!");
             }
 
+            Peca p = tab.peca(destino);
+
+            // #JogadaEspecial: Promoção...
+            if (p is Peao) {
+                if ((p.cor == Cor.Branca && destino.linha == 0) || (p.cor == Cor.Preta && destino.linha == 7)) {
+                    p = tab.retirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(tab, p.cor);
+                    tab.colocarPeca(dama, destino);
+                    pecas.Add(dama);
+                } 
+            }
+
+
             if (estahEmXeque(adversaria(jogadorAtual)))
             {
                 xeque = true;
